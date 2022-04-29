b4d7b1fd223c326b3c9b25c7bbf275bd96942525
Hannah Bush <heb.rutgers@gmail.com>
2022-04-29 02:01:39 -0400

    view queries added

--- index.js
+++ index.js
@@ -71,3 +71,64 @@ const promptUser = () => {
     });
 };
 
+//--------- VIEWS ---------
+
+//View all employees
+const viewAllEmployees = () => {
+  let sql = `SELECT employee.id,
+            employee.first_name,
+            employee.last_name,
+            role.title,
+            department.name AS 'department',
+            role.salary
+            FROM employee, role, department
+            WHERE department.id = role.department_id
+            AND role.id = employee.role_id
+            ORDER BY employee.id ASC`;
+  db.query(sql, (error, response) => {
+    if(error) throw error;
+    console.log('Current Employees:');
+    console.table(response);
+    promptUser();
+  });
+};
+
+//View all roles
+const viewAllRoles = () => {
+  const sql = `SELECT role.id, role.title, department.name AS department
+              FROM role
+              INNER JOIN department ON role.department_id = department.id`;
+  db.query(sql, (error, response) => {
+    if (error) throw error;
+    console.log(`Current Employee Roles:`);
+    response.forEach((role) => { console.log(role.title); });
+    promptUser();
+  });
+};
+
+//View all departments
+const viewAllDepartments = () => {
+  const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+  db.query(sql, (error, response) => {
+    if (error) throw error;
+    console.log(`All Departments:`);
+    console.table(response);
+    promptUser();
+  });
+};
+
+//View all employees by department
+const viewEmployeesByDepartment = () => {
+  const sql = `SELECT employee.first_name, 
+              employee.last_name, 
+              department.name AS department
+              FROM employee 
+              LEFT JOIN role ON employee.role_id = role.id 
+              LEFT JOIN department ON role.department_id = department.id`;
+  db.query(sql, (error, response) => {
+    if (error) throw error;
+    console.log(`Employees by Department:`);
+    console.table(response);
+    promptUser();
+  });
+};
